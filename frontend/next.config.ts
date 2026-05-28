b7ad7e1772ad27a1a45e2fd588455009be917179
arnavkumar2k5 <144643023+arnavkumar2k5@users.noreply.github.com>
2026-05-28 14:48:58 +0530

    updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
+import { TruckElectric } from 'lucide-react';
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: { typedRoutes: false },
+  typedRoutes: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'http://localhost:5000',
